2d81690b054a548b2ba39d7c9a5bf54364f251fe
jkworks <info@jilliankayworks.com>
2022-02-22 19:50:36 -0700

    removed bootstrap from contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,50 +1,37 @@
-
-import {Container, Row, Col} from 'react-bootstrap'
-import Phone from '../images/callivy.png'
-import Location from '../images/location1.png'
-import './Main.css'
+import Phone from "../images/callivy.png";
+import Location from "../images/location1.png";
+import "./Contact.css";
 
 const buttonStyle = {
-  fontFamily: 'Josefin Sans', 
-  backgroundColor: 'pink',
-  marginLeft: '.5rem',
-  padding: '5px'
+  fontFamily: "Josefin Sans",
+  backgroundColor: "pink",
+  marginLeft: ".5rem",
+  padding: "5px",
+};
 
-}
-
-
-export default function Contact(){
-    return (
-        <div className="container">
-
-<h1>Contact</h1>
-<Container>
-  <Row>
-    <Col sm>
-      <img src={Phone} alt="" />
-      
-      <Row>
-      {" "}
-      <a
-        href="tel:3035043536"
-        onclick="ga('send', 'event', { eventCategory: 'Contact', eventAction: 'Call', eventLabel: 'Mobile Button'});"
-      >
-       <button style={buttonStyle}>303-504-3536</button>
-       
-      </a>
-      </Row>
-    </Col> 
-
-    <Col sm>  
-    <img style={{marginLeft:"40px"}}src={Location} alt="" />
-   <Row> <p style={{marginTop: "85px", textAlign:"center"}}>6300 E Hampden Ave Unit G, Denver, CO 80222</p></Row>
-    </Col>
-
-  </Row>
-
-</Container>
-          
-         
-              </div>
-    )
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <div className="contact-content">
+        <div>
+          <img className="contact-img" src={Phone} alt="" />
+            {" "}
+            <a
+              href="tel:3035043536"
+              onclick="ga('send', 'event', { eventCategory: 'Contact', eventAction: 'Call', eventLabel: 'Mobile Button'});"
+            >
+              <button style={buttonStyle}>303-504-3536</button>
+            </a>
+            </div>
+            <div>
+          <img className="contact-img" style={{ marginLeft: "40px" }} src={Location} alt="" />
+            {" "}
+            <p style={{ marginTop: "85px", textAlign: "center" }}>
+              6300 E Hampden Ave Unit G, Denver, CO 80222
+            </p>
+            </div>
+      </div>
+    </div>
+  );
 }
